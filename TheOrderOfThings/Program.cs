621ac7c0b6a4b72d38910cded3c3751bb022944e
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-14 14:51:12 -0500

    The Order Of Things exercise

---
+++ TheOrderOfThings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOrderOfThings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            String opinion, size, age, shape, color, origin, material, purpose;
+            String noun;
+
+            number = 5;
+            opinion = "AWESOME";
+            size = "teensy-weensy";
+            age = "new";
+            shape = "oblong";
+            color = "yellow";
+            origin = "Martian";
+            material = "platinum";
+            purpose = "good";
+
+            noun = "dragons";
+
+            // Using the + with strings doesn't add -- it concatenates! (sticks them together)
+            Console.WriteLine(number + " " + opinion + " " + size + " " + age + " " + shape + " " + color
+                    + " " + origin + " " + material + " " + purpose + " " + noun);
+        }
+    }
+}
