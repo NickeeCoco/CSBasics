0a5f976c8d8bb444fe5c60f8a05c3214093ba3e3
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-14 14:50:09 -0500

    MoreBuckets More Fun exercise

---
+++ MoreBucketsMoreFun/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreBucketsMoreFun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare ALL THE THINGS
+            // (Usually it's a good idea to declare them at the beginning of the program)
+            int butterflies, beetles, bugs;
+
+            // Now assign values to some of the variables
+            butterflies = 5;
+            beetles = 9;
+
+            // Next, we use the variables to create output
+            bugs = butterflies + beetles;
+            Console.WriteLine("There are only " + butterflies + " butterflies,");
+            Console.WriteLine("but there are " + bugs + " bugs in all.");
+
+            Console.WriteLine("Uh oh, my dog ate one.");
+            butterflies--;
+            Console.WriteLine("Now there are only " + butterflies + " butterflies left.");
+            Console.WriteLine("But there are still " + bugs + " bugs left...");
+            Console.WriteLine("Wait a minute!");
+            Console.WriteLine("... maybe my computer can't do math, after all!");
+        }
+    }
+}
