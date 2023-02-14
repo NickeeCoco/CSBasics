39dfe3cbb21c84f8b6a3ce4a01d235e88a487a44
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-14 15:03:23 -0500

    Best Adder Ever exercise

---
+++ BestAdderEver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestAdderEver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 10;
+            int number3 = 7;
+            int additionResult = number1 + number2 + number3;
+
+            Console.WriteLine(number1 + " + " + number2 + " + " + number3 + " = " + additionResult);
+        }
+    }
+}
